68e2fc8b180abce7eb0956c358f479b6bd340a6f
Gashi, Arber AR_MDE-HR-AA <Arber.Gashi@mueller-milch.de>
2024-08-18 23:58:49 +0200

    lkjölkj

---
+++ Exceptions/Extensions/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions.Extensions
+{
+    public static class Extension
+    {
+
+        public static bool checkIfReal(this string Input)
+        {
+            if(File.Exists(Input)) 
+                    return true; 
+
+            return false;
+        }
+
+
+
+
+
+    }
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,8 +1,15 @@
-﻿do
+﻿using Exceptions.Extensions;
+bool success;
+
+string path = @"C:\Users\Arber.Gashi\OneDrive - Müller Service GmbH\Desktop";
+
+do
 {
     Console.WriteLine("Enter the name of the file you want to read:");
-    
-
-} while()
+    string? userInput = Console.ReadLine();
+    success = userInput.checkIfReal();
+    if (success)
+        Console.WriteLine("Es ist da!");
+} while (success == false);
 
 
