b3651ea1e1699bc0ed1e681c00761bf1ba1a39ca
Gashi, Arber AR_MDE-HR-AA <Arber.Gashi@mueller-milch.de>
2024-08-19 18:03:24 +0200

    some implementations

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,13 +1,58 @@
 ﻿using Exceptions.Extensions;
-bool success;
+using System.Text.Json;
 
-do
+
+datagameparser juststart = new datagameparser();
+juststart.mainMenu();
+Console.ReadKey();
+
+public class datagameparser
 {
-    Console.WriteLine("Enter the name of the file you want to read:");
-    string? userInput = Console.ReadLine();
-    success = userInput.checkIfReal();
-    if (success)
-        Console.WriteLine("Es ist da!");
-} while (success == false);
+
+    public bool success;
+
+    public void mainMenu()
+    {
+        //do
+        //{
 
 
+            Console.WriteLine("Enter the name of the file you want to read:");
+            string? userInput = Console.ReadLine();
+            try
+            {
+            var jsonString = JsonSerializer.Deserialize<List<string>>(userInput);
+
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("File wurde nicht gefunden", ex.Message);
+
+            }
+
+
+            //success = userInput.checkIfReal();
+            //if (success)
+            //    Console.WriteLine("Es ist da!");
+
+
+
+        //} while (success == false);
+
+        Console.ReadKey();
+    }
+
+
+
+
+
+}
+
+
+public class Game
+{
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+    public double Rating { get; set; }
+
+}
